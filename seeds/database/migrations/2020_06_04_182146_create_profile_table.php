f9d3444709ca0e3dc5e388d3cedeedc2fec2c95c
FORGE-JITA\ktokt <ulfnew@gmail.com>
2020-06-05 01:42:14 +0500

    Add profile table

---
+++ seeds/database/migrations/2020_06_04_182146_create_profile_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->text('description');
+            $table->string('email_for_news');
+            $table->dateTime('last_visit_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile');
+    }
+}
